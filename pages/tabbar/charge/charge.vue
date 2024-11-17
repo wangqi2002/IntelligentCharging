5989124dd63221041fc3547263caecd17a4c45fd
wangqi <172209706@qq.com>
2024-11-17 23:49:16 +0800

    yaokong update2

--- pages/tabbar/charge/charge.vue
+++ pages/tabbar/charge/charge.vue
@@ -25,274 +25,297 @@
 			</view>
 		</view>
 		<view class="options-wrapper">
-			<button class="guiwei"></button>
-			<button class="confirm"></button>
+			<button class="guiwei option-btn">归位</button>
+			<button class="confirm option-btn">确认</button>
 		</view>
 	</view>
 </template>
 <script>
-import menuData from '@/config/menuData.js';
+	import menuData from '@/config/menuData.js';
 
-export default {
-	data() {
-		return {};
-	},
-	async onLoad() {},
-	onShow() {},
-	onReady() {},
-	mounted() {},
-	methods: {}
-};
+	export default {
+		data() {
+			return {};
+		},
+		async onLoad() {},
+		onShow() {},
+		onReady() {},
+		mounted() {},
+		methods: {}
+	};
 </script>
 
 <style lang="scss">
-page {
-	background-color: #f7f7f7;
-}
-
-.title-wrapper {
-	position: fixed;
-	top: 0;
-	padding-top: calc(var(--status-bar-height) + 50rpx);
-	height: calc(var(--status-bar-height) + 110rpx);
-	width: 100%;
-	z-index: 2;
-	.title {
-		font-family: 'Helvetica Neue', Helvetica, Arial, 'PingFang SC', 'Hiragino Sans GB', 'Heiti SC', 'Microsoft YaHei', 'WenQuanYi Micro Hei', sans-serif;
-		font-size: $font-lg;
-		text-align: center;
+	page {
+		background-color: #f7f7f7;
 	}
-}
 
-.video-wrapper {
-	width: 100%;
-	padding: calc(110rpx + var(--status-bar-height)) 0 0;
-	.video-card {
+	.title-wrapper {
+		position: fixed;
+		top: 0;
+		padding-top: calc(var(--status-bar-height) + 50rpx);
+		height: calc(var(--status-bar-height) + 110rpx);
 		width: 100%;
-	}
-}
-
-.control-wrapper {
-	cursor: pointer;
-	position: relative;
-	width: 550rpx;
-	height: 550rpx;
-	max-width: 600rpx;
-	max-height: 600rpx;
-	min-width: 300rpx;
-	min-height: 300rpx;
-	margin: 0 auto;
-	border-radius: 50%;
-
-	.control-btn {
-		position: absolute;
-		width: 38%;
-		height: 38%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		// border: 5rpx solid #78aee4;
-		box-sizing: border-box;
-		background-color: #78aee4;
-		transition: all 0.3s linear;
-	}
-
-	.control-btn:active {
-		color: black;
-		background: #63a300;
-	}
-
-	.control-btn:after {
-		content: '';
-		position: absolute;
-		width: 60%;
-		height: 60%;
 		z-index: 2;
+
+		.title {
+			font-family: 'Helvetica Neue', Helvetica, Arial, 'PingFang SC', 'Hiragino Sans GB', 'Heiti SC', 'Microsoft YaHei', 'WenQuanYi Micro Hei', sans-serif;
+			font-size: $font-lg;
+			text-align: center;
+		}
 	}
 
-	.control-btn:before {
-		content: '';
+	.video-wrapper {
+		width: 100%;
+		padding: calc(110rpx + var(--status-bar-height)) 0 0;
+
+		.video-card {
+			width: 100%;
+		}
+	}
+
+	.control-wrapper {
+		cursor: pointer;
 		position: relative;
-		display: block;
-		width: 30rpx;
-		height: 30rpx;
-		border-top: 10rpx solid #fff;
-		border-right: 10rpx solid #fff;
-		border-radius: 0 4rpx 0 0;
-		box-sizing: border-box;
-		z-index: 3;
-	}
-
-	.control-top {
-		top: 0;
-		left: 50%;
-		transform: translateX(-50%) rotate(-45deg);
-		border-radius: 4rpx 100% 4rpx 4rpx;
-	}
-
-	.control-top:before {
-		transform: translate(30%, -25%);
-	}
-
-	.control-top:after {
-		left: 0;
-		bottom: 0;
-		border-radius: 0 100% 0 0;
-	}
-
-	.control-bottom {
-		left: 50%;
-		bottom: 0;
-		transform: translateX(-50%) rotate(45deg);
-		border-radius: 4rpx 4rpx 100% 4rpx;
-	}
-
-	.control-bottom:before {
-		transform: translate(25%, 25%) rotate(90deg);
-	}
-
-	.control-bottom:after {
-		top: 0;
-		left: 0;
-		border-radius: 0 0 100% 0;
-	}
-
-	.control-left {
-		top: 50%;
-		left: 0;
-		transform: translateY(-50%) rotate(45deg);
-		border-radius: 4rpx 4rpx 4rpx 100%;
-	}
-
-	.control-left:before {
-		transform: translate(-25%, 30%) rotate(180deg);
-	}
-
-	.control-left:after {
-		right: 0;
-		top: 0;
-		border-radius: 0 0 0 100%;
-	}
-
-	.control-right {
-		top: 50%;
-		right: 0;
-		transform: translateY(-50%) rotate(45deg);
-		border-radius: 4rpx 100% 4rpx 4rpx;
-	}
-
-	.control-right:before {
-		transform: translate(30%, -25%);
-	}
-
-	.control-right:after {
-		left: 0;
-		bottom: 0;
-		border-radius: 0 100% 0 0;
-	}
-
-	.control-round {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		width: 280rpx;
-		height: 280rpx;
-		background: #fff;
+		width: 550rpx;
+		height: 550rpx;
+		max-width: 600rpx;
+		max-height: 600rpx;
+		min-width: 300rpx;
+		min-height: 300rpx;
+		margin: 0 auto;
 		border-radius: 50%;
 
-		.control-round-up {
+		.control-btn {
 			position: absolute;
-			top: 35%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			width: 140rpx;
-			height: 70rpx;
-			// border: 5rpx solid #fff;
-			border-radius: 140rpx 140rpx 0 0;
-		}
-
-		.control-round-up:active {
-			color: black;
-			background: yellow;
-		}
-
-		.control-round-up:before {
-			transform: translate(0, 50%) rotate(-45deg);
-		}
-		.control-round-down {
-			position: absolute;
-			top: 65%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			width: 140rpx;
-			height: 70rpx;
-			border-radius: 0 0 140rpx 140rpx;
-		}
-
-		.control-round-down:active {
-			color: black;
-			background: yellow;
-		}
-
-		.control-round-down:before {
-			transform: translate(0, -50%) rotate(135deg);
-		}
-	}
-
-	.control-rotate {
-		width: 100%;
-		height: 160rpx;
-		position: absolute;
-		top: 90%;
-		display: flex;
-		justify-content: space-between;
-		.control-rotate-btn {
-			position: absolute;
-			width: 160rpx;
-			height: 160rpx;
+			width: 38%;
+			height: 38%;
 			display: flex;
 			align-items: center;
 			justify-content: center;
+			// border: 5rpx solid #78aee4;
 			box-sizing: border-box;
 			background-color: #78aee4;
 			transition: all 0.3s linear;
-			border-radius: 100%;
-
-			.rotate-icon {
-				width: 30%;
-				height: 30%;
-				position: absolute;
-				top: 50%;
-				left: 50%;
-				transform: translate(-50%, -50%);
-			}
 		}
-		.control-rotate-btn:active {
+
+		.control-btn:active {
 			color: black;
 			background: #63a300;
 		}
-		.control-rotate-left {
-			position: absolute;
-			left: 0;
-		}
-		.control-rotate-right {
-			position: absolute;
-			right: 0;
-		}
-	}
-}
 
-/* DarkMode 下的样式 start */
-@media (prefers-color-scheme: dark) {
-	page {
-		background-color: #121425;
+		.control-btn:after {
+			content: '';
+			position: absolute;
+			width: 60%;
+			height: 60%;
+			z-index: 2;
+		}
+
+		.control-btn:before {
+			content: '';
+			position: relative;
+			display: block;
+			width: 30rpx;
+			height: 30rpx;
+			border-top: 10rpx solid #fff;
+			border-right: 10rpx solid #fff;
+			border-radius: 0 4rpx 0 0;
+			box-sizing: border-box;
+			z-index: 3;
+		}
+
+		.control-top {
+			top: 0;
+			left: 50%;
+			transform: translateX(-50%) rotate(-45deg);
+			border-radius: 4rpx 100% 4rpx 4rpx;
+		}
+
+		.control-top:before {
+			transform: translate(30%, -25%);
+		}
+
+		.control-top:after {
+			left: 0;
+			bottom: 0;
+			border-radius: 0 100% 0 0;
+		}
+
+		.control-bottom {
+			left: 50%;
+			bottom: 0;
+			transform: translateX(-50%) rotate(45deg);
+			border-radius: 4rpx 4rpx 100% 4rpx;
+		}
+
+		.control-bottom:before {
+			transform: translate(25%, 25%) rotate(90deg);
+		}
+
+		.control-bottom:after {
+			top: 0;
+			left: 0;
+			border-radius: 0 0 100% 0;
+		}
+
+		.control-left {
+			top: 50%;
+			left: 0;
+			transform: translateY(-50%) rotate(45deg);
+			border-radius: 4rpx 4rpx 4rpx 100%;
+		}
+
+		.control-left:before {
+			transform: translate(-25%, 30%) rotate(180deg);
+		}
+
+		.control-left:after {
+			right: 0;
+			top: 0;
+			border-radius: 0 0 0 100%;
+		}
+
+		.control-right {
+			top: 50%;
+			right: 0;
+			transform: translateY(-50%) rotate(45deg);
+			border-radius: 4rpx 100% 4rpx 4rpx;
+		}
+
+		.control-right:before {
+			transform: translate(30%, -25%);
+		}
+
+		.control-right:after {
+			left: 0;
+			bottom: 0;
+			border-radius: 0 100% 0 0;
+		}
+
+		.control-round {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			width: 280rpx;
+			height: 280rpx;
+			background: #fff;
+			border-radius: 50%;
+
+			.control-round-up {
+				position: absolute;
+				top: 35%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				width: 140rpx;
+				height: 70rpx;
+				// border: 5rpx solid #fff;
+				border-radius: 140rpx 140rpx 0 0;
+			}
+
+			.control-round-up:active {
+				color: black;
+				background: yellow;
+			}
+
+			.control-round-up:before {
+				transform: translate(0, 50%) rotate(-45deg);
+			}
+
+			.control-round-down {
+				position: absolute;
+				top: 65%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				width: 140rpx;
+				height: 70rpx;
+				border-radius: 0 0 140rpx 140rpx;
+			}
+
+			.control-round-down:active {
+				color: black;
+				background: yellow;
+			}
+
+			.control-round-down:before {
+				transform: translate(0, -50%) rotate(135deg);
+			}
+		}
+
+		.control-rotate {
+			width: 100%;
+			height: 160rpx;
+			position: absolute;
+			top: 90%;
+			display: flex;
+			justify-content: space-between;
+
+			.control-rotate-btn {
+				position: absolute;
+				width: 160rpx;
+				height: 160rpx;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				box-sizing: border-box;
+				background-color: #78aee4;
+				transition: all 0.3s linear;
+				border-radius: 100%;
+
+				.rotate-icon {
+					width: 30%;
+					height: 30%;
+					position: absolute;
+					top: 50%;
+					left: 50%;
+					transform: translate(-50%, -50%);
+				}
+			}
+
+			.control-rotate-btn:active {
+				color: black;
+				background: #63a300;
+			}
+
+			.control-rotate-left {
+				position: absolute;
+				left: 0;
+			}
+
+			.control-rotate-right {
+				position: absolute;
+				right: 0;
+			}
+		}
 	}
-}
+
+	.options-wrapper {
+		width: 100%;
+		height: 100rpx;
+		margin-top: 120rpx;
+		display: flex;
+
+		.option-btn {
+			width: 220rpx;
+			height: 100rpx;
+			background-color: #78aee4;
+			color: #fff;
+			line-height: 110rpx;
+			font-size: $font-sl;
+		}
+	}
+
+	/* DarkMode 下的样式 start */
+	@media (prefers-color-scheme: dark) {
+		page {
+			background-color: #121425;
+		}
+	}
 </style>
