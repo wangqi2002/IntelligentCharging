81247b6e68b53f01c4cf6aeadae00e5c3b7ed231
wq <602113@gree.com.cn>
2024-12-13 11:28:02 +0800

    mqtt

---
+++ server/mqtt/mqtt_pub.py
@@ -0,0 +1,60 @@
+# python 3.8
+
+import random
+import time
+
+from paho.mqtt import client as mqtt_client
+
+
+broker = 'broker.emqx.io'
+port = 1883
+topic = "python/mqtt"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+username = 'emqx'
+password = '**********'
+
+# TCP端口连接
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, client_id)
+    # client.tls_set(ca_certs='./server-ca.crt')
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+# 订阅主题
+def publish(client):
+    msg_count = 0
+    while True:
+        time.sleep(1)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        msg_count += 1
+
+# 取消订阅
+def unsubscribe(client: mqtt_client):
+    client.on_message = None
+    client.unsubscribe(topic)
+
+# 断开连接
+def disconnect(client: mqtt_client):
+    client.loop_stop()
+    client.disconnect()
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
---
+++ server/mqtt/mqtt_sub.py
@@ -0,0 +1,47 @@
+# python3.8
+
+import random
+
+from paho.mqtt import client as mqtt_client
+
+
+broker = 'broker.emqx.io'
+port = 1883
+topic = "python/mqtt"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+username = 'emqx'
+password = '**********'
+
+# TCP端口连接
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, client_id)
+    # client.tls_set(ca_certs='./server-ca.crt')
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+# 发布消息
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+    client.subscribe(topic)
+    client.on_message = on_message
+
+# 断开连接
+def disconnect(client: mqtt_client):
+    client.loop_stop()
+    client.disconnect()
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
